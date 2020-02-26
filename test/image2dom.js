e5e89726ce113a05cb67577230e4ffd652251d38
guimao dai <wyyxdgm@163.com>
2020-02-26 09:07:20 +0800

    tmp image2dom.js

--- test/image2dom.js
+++ test/image2dom.js
@@ -19,13 +19,13 @@ function ri(res, i) {
   const model = imageModel.analysModel();
   const tt = 'aa' || moment().format('HHmmss');
   imageModel.drawArea(path.join(__dirname, `./output-${pad(i,2)}-${tt}.png`));
-  imageModel.drawEmptyArea(path.join(__dirname, `./output-${pad(i,2)}-${tt}-empty.png`));
+  // imageModel.drawEmptyArea(path.join(__dirname, `./output-${pad(i,2)}-${tt}-empty.png`));
 }
 
 imageToRgbaMatrix(path.join(__dirname, _path)).then((res) => {
-  // for (let i = 1; i < 30; i++) {
-  //   ri(res, i);
-  // }
-  ri(res, 11);
+  for (let i = 1; i < 30; i++) {
+    ri(res, i);
+  }
+  // ri(res, 11);
 
 });
---
+++ test/sketch.js
@@ -0,0 +1,10 @@
+var sketch = require('sketch/dom')
+var async = require('sketch/async')
+var DataSupplier = require('sketch/data-supplier')
+var UI = require('sketch/ui')
+var Settings = require('sketch/settings')
+
+// a more convenient require which exposes everything (might be a bit slower)
+// var sketch = require('sketch')
+
+// var Document = require('sketch/dom').Document
