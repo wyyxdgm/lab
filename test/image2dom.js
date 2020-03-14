b2670b98a2acfef226d2b1787964c20fa3ecf1c7
guimao dai <wyyxdgm@163.com>
2020-03-14 15:57:01 +0800

    左上角+圈圈寻找方案

--- test/image2dom.js
+++ test/image2dom.js
@@ -1,12 +1,11 @@
 const imageToRgbaMatrix = require('image-to-rgba-matrix');
 const moment = require('moment');
-const { pad } = require('../lib/util');
+// const util = require('../lib/util');
+const { pad, fixPath } = require('../lib/util');
 // const { fromPng } = require('@rgba-image/png');
 // const fs = require('fs');
 // const png = fs.readFileSync('in.png')
 
-
-const getPath = p => path.join(__dirname, p);
 
 let fs = require('fs');
 let path = require('path');
@@ -15,16 +14,16 @@ const ImageModel = require('../lib/ImageModel');
 const Shape = require('../lib/Shape');
 const ShapeRect = require('../lib/ShapeRect');
 
-const input2File = getPath('./input/input2.png');
-const tagFile = getPath('./input/tag.png');
-const tagsFile = getPath('./input/tags.png');
-const pageFile = getPath('./input/help-center@70%.png');
+const input2File = fixPath('test/input/input2.png');
+const tagFile = fixPath('test/input/tag.png');
+const tagsFile = fixPath('test/input/tags.png');
+const pageFile = fixPath('test/input/help-center@70%.png');
 
 
 function ri(res, i) {
   const imageModel = new ImageModel(res, i);
   const model = imageModel.analysModel();
-  imageModel.drawArea(getPath(`./output/${pad(i,2)}.png`));
+  imageModel.drawArea(fixPath(`test/output/${pad(i,2)}.png`));
   // imageModel.drawEmptyArea(path.join(__dirname, `./output-${pad(i,2)}-${tt}-empty.png`));
 }
 
@@ -34,12 +33,12 @@ Shape.newInstance(tagFile).then(shapeModel => {
   ImageModel.newInstance(tagsFile).then(pageModel => {
     const points = pageModel.searchShape(shapeModel);
     console.log(`points matched size: ${points.length}`);
-    // console.log(points);
+    console.log(points);
     let shapeRects = pageModel.getReacAreaAroundPoints(points, shapeModel);
     console.log('shapeRects', shapeRects.filter(sr => sr.w > 0));
     shapeRects = shapeRects.filter(sr => sr.w > 0);
-    // pageModel.sliceRectsToFile(shapeRects, getPath('./output/{name}.png'));
-    pageModel.drawRectsOnSelfToFile(shapeRects, getPath('./output/all.png'));
+    pageModel.sliceRectsToFile(shapeRects, fixPath('test/output/{name}.png'));
+    pageModel.drawRectsOnSelfToFile(shapeRects, fixPath('test/output/all.png'));
   })
 })
 
