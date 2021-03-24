f8192114091ff1074773654ab05d9b887cb3ad5d
Damo <daiguimao@sightp.com>
2021-03-24 11:20:32 +0800

    animation&&parseParam

---
+++ js/parseParam.js
@@ -0,0 +1,17 @@
+module.exports = function parseParam(param, key) {
+  var paramStr = "";
+  if (['string', 'number', 'boolean'].indexOf(typeof param) > -1) {
+    paramStr += `&${key}=${param}`;
+  } else if (param instanceof Array) {
+    param.forEach((p, i) => {
+      var k = key == null ? i : `${key}[${i}]`;
+      paramStr += '&' + parseParam(p, k);
+    });
+  } else { // object
+    Object.keys(param).forEach((i) => {
+      var k = !key ? i : `${key}.${i}`;
+      paramStr += '&' + parseParam(param[i], k);
+    })
+  }
+  return paramStr.substr(1);
+};
