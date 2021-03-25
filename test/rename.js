a26d30d53ed62e5be754361272bece8756544b09
Damo <daiguimao@sightp.com>
2021-03-25 11:01:48 +0800

    rename for activity

--- test/rename.js
+++ test/rename.js
@@ -1,23 +1,42 @@
 const fs = require('fs');
 const path = require('path');
-const dir = '/Users/damo/workspace/sightp.com/ARTravel-wxapp/remote/images/map-markers/';
+// 地图图标
+// const dir = '/Users/damo/workspace/sightp.com/ARTravel-wxapp/remote/images/map-markers/';
+// const conifg = [
+//   ['AR', 'ar'],
+//   ['停车场', 'parking'],
+//   ['售票处', 'ticket-office'],
+//   ['景点', 'scenic'],
+//   ['洗手间', 'restroom'],
+//   ['美食', 'food'],
+//   [/购物/, 'shopping'],
+//   [/^(.*)备份\s2\@2x(.*)$/, '$1-recommended.png'],
+//   [/^(.*)备份\@2x(.*)$/, '$1-recommended-arrived.png'],
+//   [/^(.*)大点位(.*)\.png$/, '$1setting-icon$2-lg.png'],
+//   [/^(.*)点位(.*)\.png$/, '$1setting-icon$2.png'],
+//   ['@2x', ''],
+//   ['iconx', 'icon']
+// ]
+
+// 活动图标
+const dir = '/Users/damo/Downloads/v2.0/活动图标/';
+// i-大点位-活动-捕鱼@2x.png
+// i-点位-活动-捕鱼@2x.png
+const conifg = [
+  ['鸭子', 'duck'],
+  ['拼图', 'puzzle'],
+  ['萌宠', 'pet'],
+  ['成就', 'achievement'],
+  [/捕鱼/, 'fishing'],
+  // [/^(.*)备份\s2\@2x(.*)$/, '$1-recommended.png'],
+  // [/^(.*)备份\@2x(.*)$/, '$1-recommended-arrived.png'],
+  [/^(.*)大点位-活动(.*)\.png$/, '$1activity-icon$2-lg.png'],
+  [/^(.*)点位-活动(.*)\.png$/, '$1activity-icon$2.png'],
+  ['@2x', ''],
+]
 fs.readdir(dir, (err, source) => {
   console.log(source)
-  conifg = [
-    ['AR', 'ar'],
-    ['停车场', 'parking'],
-    ['售票处', 'ticket-office'],
-    ['景点', 'scenic'],
-    ['洗手间', 'restroom'],
-    ['美食', 'food'],
-    [/购物/, 'shopping'],
-    [/^(.*)备份\s2\@2x(.*)$/, '$1-recommended.png'],
-    [/^(.*)备份\@2x(.*)$/, '$1-recommended-arrived.png'],
-    [/^(.*)大点位(.*)\.png$/, '$1setting-icon$2-lg.png'],
-    [/^(.*)点位(.*)\.png$/, '$1setting-icon$2.png'],
-    ['@2x', ''],
-    ['iconx', 'icon']
-  ]
+
   // SETTING_ICON
   // SETTING_ICON_LG
   // SETTING_ICON_RECOMMENDED
